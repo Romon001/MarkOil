770571b99cf7358f4d3462779dd87e8857f8cca5
Romon001 <Roman.ruchin@mail.ru>
2021-03-16 22:36:38 +0300

    Добвалена отрисовка графика ИТК по точкам

--- MarkOil/Forms/Form3.Designer.cs
+++ MarkOil/Forms/Form3.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
@@ -68,16 +68,18 @@
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(505, 47);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(244, 267);
             this.chart1.TabIndex = 3;
             this.chart1.Text = "chart1";
@@ -90,6 +92,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- MarkOil/Forms/Form3.cs
+++ MarkOil/Forms/Form3.cs
@@ -24,7 +24,62 @@ namespace MarkOil
 
         private void Form3_Load(object sender, EventArgs e)
         {
+            dataGridView1.Rows.Add("66", "0");
+            dataGridView1.Rows.Add("80", "3");
+            dataGridView1.Rows.Add("100", "7");
+            dataGridView1.Rows.Add("120", "10");
+            dataGridView1.Rows.Add("140", "14");
+            dataGridView1.Rows.Add("160", "19");
+            dataGridView1.Rows.Add("180", "22");
+            dataGridView1.Rows.Add("200", "26");
+            dataGridView1.Rows.Add("220", "29");
+            dataGridView1.Rows.Add("240", "33");
+            dataGridView1.Rows.Add("260", "37");
+            dataGridView1.Rows.Add("280", "40");
+            dataGridView1.Rows.Add("300", "44");
+            dataGridView1.Rows.Add("320", "48");
+            dataGridView1.Rows.Add("340", "54");
+            dataGridView1.Rows.Add("350", "56");
+            dataGridView1.Rows.Add("360", "59");
 
+            dataGridView2.Rows.Add("380", "0");
+            dataGridView2.Rows.Add("390", "4");
+            dataGridView2.Rows.Add("400", "10");
+            dataGridView2.Rows.Add("410", "16");
+            dataGridView2.Rows.Add("420", "20");
+            dataGridView2.Rows.Add("430", "25");
+            dataGridView2.Rows.Add("440", "29");
+            dataGridView2.Rows.Add("450", "33");
+            dataGridView2.Rows.Add("460", "36");
+            dataGridView2.Rows.Add("470", "40");
+            dataGridView2.Rows.Add("480", "44");
+            dataGridView2.Rows.Add("690", "46");
+            dataGridView2.Rows.Add("500", "48");
+            dataGridView2.Rows.Add("510", "50");
+            dataGridView2.Rows.Add("520", "52");
+            dataGridView2.Rows.Add("530", "55");
+            dataGridView2.Rows.Add("540", "57");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = 0;
+            double y = 0;
+            chart1.Series[0].Points.Clear();
+            for(int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                x = Convert.ToDouble(dataGridView1[1, i].Value);
+                y = Convert.ToDouble(dataGridView1[0, i].Value);
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+            double buffer = x;
+            for (int i = 0; i < dataGridView2.Rows.Count-1; i++)
+            {
+                x = (100-buffer)*(0.01)*Convert.ToDouble(dataGridView2[1, i].Value) + buffer;
+                y = Convert.ToDouble(dataGridView2[0, i].Value) ;
+                chart1.Series[0].Points.AddXY(x, y);
+            }
         }
     }
 }
